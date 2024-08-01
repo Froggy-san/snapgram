76bf5223050fb05d7db486475f9c0cc7ad395e4c
Froggy-san <medoosama441@yahoo.com>
2024-08-02 01:09:56 +0300

    trying to fix the auth problem

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,10 +1,14 @@
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
+import { useUserContext } from "@/context/AuthContext";
 
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 
 function RootLayout() {
+  const { isAuthenticated } = useUserContext();
+
+  if (!isAuthenticated) return <Navigate to="/sign-in" />;
   return (
     <div className="w-full md:flex">
       <Topbar />
